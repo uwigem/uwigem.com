78148f04f714c5af39f3a05bd0c23c257defd722
Allison <allison.lee124@gmail.com>
2019-03-19 22:32:01 -0700

    create TeamPage.js and testing file

---
+++ components/TeamPage/TeamPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Constants from '../../global_constants/Constants';
+
+/**
+ * @returns {React.Component}
+ */
+const TeamPage = () => {
+    return <></>;
+}
+
+export default TeamPage;
