a0fc07c6a504aa99bb3d659b7c565a924b031558
William Kwok <wkwok1998@gmail.com>
2019-04-11 13:08:27 -0700

    Updated small thing on index.js

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
 import SideBySideContent from '../components/SideBySideContent/SideBySideContent';
 import '../global_constants/Global_Styles.css';
-// const leftWidth = 500;
 //import Dubs from "../static/dubs.jpeg";
 
 class LeftChild extends React.Component {
     render() {
-        return <h1>LeftChild</h1>;
+        return <h1 style={{backgroundColor: "blue"}}>LeftChild</h1>;
     }
 };
 
