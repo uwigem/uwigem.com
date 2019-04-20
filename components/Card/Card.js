cc90155957a33ab9248c6fb02a91424692703712
zhouyifan0904 <zhouyf98@uw.edu>
2019-04-20 10:03:51 -0700

    updated navbar

--- components/Card/Card.js
+++ components/Card/Card.js
@@ -2,92 +2,25 @@ import React from 'react';
 import Constants from '../../global_constants/Constants';
 import 'style.css';
 
-/**
- * @returns {React.Component}
- */
-const NavigationBar = () => {
-	return <></>
-}
 
+export default class Card extends Component {
 
-/*
-props:
-	title
-	bodyText
-	link
-*/
-export class Card extends Component {
-	
 	constructor(props) {
 		super(props);
-	}
-	
-	render() {
-		return (
-			<div className='card-whole'>
-				<CardTitle title={this.props.title} />
-				<CardBody bodyText={this.props.bodyText} />
-				<LearnMoreButton link={this.props.link} />
-			</div>
-		)
-	}
-}
-
-
-/*
-props:
-	title
-*/
-class CardTitle extends Component {
-	constructor(props) {
-		super(props);
+		this.state ={
+			hover: false
+		};
 	}
 
 	render() {
 		return (
-			<div className='card-title'>
-				<h2>
-					{this.props.title}
-				</h2>
-			</div>
-		)
-	}
-}
-
-/*
-props:
-	bodyText
-*/
-class CardBody extends Component {
-	constructor(props) {
-		super(props);
-	}
-
-	render() {
-		return (
-			<div className='card-body'>
-				<p>
-					{this.props.bodyText}
-				</p>
-			</div>
-		)
-	}
-}
-
-
-/*
-props:
-	link
-*/
-class LearnMoreButton extends Component {
-	constructor(props) {
-		super(props);
-	}
-	
-	render() {
-		return (
-			<div className='learn-button'>
-				<a href={this.props.link}>Learn More</a>
+			<div className="card">
+				<div className="card-title">
+				</div>
+				<div className="card-body">
+				</div>
+				<div className="card-actions">
+				</div>
 			</div>
 		)
 	}
--- components/NavigationBar/NavigationBar.js
+++ components/NavigationBar/NavigationBar.js
@@ -2,49 +2,50 @@ import React from 'react';
 import Constants from '../../global_constants/Constants';
 import 'style.css';
 
-/**
- * @returns {React.Component}
- */
-const NavigationBar = () => {
-	return <></>
-}
+export default class NavBar extends Component {
+  
+  render() {
+    // placeholder
+    let links = ["Link1", "Link2", "Link3", "Link4"];
 
-export class NavBar extends Component {
-	
-	render() {
+    links = links.map((link) => {
+      return (
+        <Link link="#" name={link} />
+      );
+    })
 
-		return (
-			<div className="nav-box">
-				<img src="logo.png" height="30" width="30px" />
-				<Link name="PROJECTS" link="#"/>
-				<Link name="ABOUT" link="#"/>
-				<Link name="GET INVOLVED" link="#"/>
-			</div>
-		)
-	}
+    return (
+      <div className="navbar">
+        <div className="navbar-logo-container">
+          
+        </div>
+        <div className="navbar-link-container">
+          {links}
+        </div>
+      </div>
+    )
+  }
 }
 
 /*
 props:
-	link: link/react-router address
-	name: display name of the link
+  link: link/react-router address
+  name: display name of the link
 */
 class Link extends Component {
 
-	constructor(props) {
-		super(props);
-		this.state = {
-			hover: false
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      hover: false
+    };
+  }
 
-	render() {
-		return (
-			<div className="nav-link">
-				<a ref={this.props.link}>{this.props.name}</a>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="navlink-container">
+        <a className="navlink" ref={this.props.link}>{this.props.name}</a>
+      </div>
+    );
+  }
 }
-
-export default NavigationBar
