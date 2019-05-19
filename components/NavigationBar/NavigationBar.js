1190d98e72a3036fbc19f7624ccd61126952e342
zhouyifan0904 <zhouyf98@uw.edu>
2019-05-18 22:10:59 -0700

    updated navbar

--- components/NavigationBar/NavigationBar.js
+++ components/NavigationBar/NavigationBar.js
@@ -3,49 +3,73 @@ import Constants from '../../global_constants/Constants';
 import 'style.css';
 
 export default class NavBar extends Component {
-  
-  render() {
-    // placeholder
-    let links = ["Link1", "Link2", "Link3", "Link4"];
+	
+	render() {
+		// placeholder
+		let links = ["Home", "Project", "Human Practices", "Safety", "People", "Judging"];
 
-    links = links.map((link) => {
-      return (
-        <Link link="#" name={link} />
-      );
-    })
+		links = links.map((link) => {
+			return (
+				<Link link="#" name={link} />
+			);
+		})
 
-    return (
-      <div className="navbar">
-        <div className="navbar-logo-container">
-          
-        </div>
-        <div className="navbar-link-container">
-          {links}
-        </div>
-      </div>
-    )
-  }
+		return (
+			<div className="navbar">
+				<div className="navbar-logo-container">
+					
+				</div>
+				<div className="navbar-link-container">
+					{links}
+				</div>
+			</div>
+		)
+	}
 }
 
 /*
 props:
-  link: link/react-router address
-  name: display name of the link
+	link: link/react-router address
+	name: display name of the link
+	special: dark background if true
 */
 class Link extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      hover: false
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			hover: false
+		};
+	}
 
-  render() {
-    return (
-      <div className="navlink-container">
-        <a className="navlink" ref={this.props.link}>{this.props.name}</a>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="navlink-container">
+				<a className="navlink" ref={this.props.link}>{this.props.name}</a>
+			</div>
+		);
+	}
+}
+
+class Card extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state ={
+			hover: false
+		};
+	}
+
+	render() {
+		return (
+			<div className="card">
+				<div className="card-title">
+				</div>
+				<div className="card-body">
+				</div>
+				<div className="card-actions">
+				</div>
+			</div>
+		)
+	}
 }
